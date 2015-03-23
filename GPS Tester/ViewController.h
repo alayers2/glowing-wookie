025a7fb1a9fb02b791ca01dbc431edc30915274d
Chris Bennett <FORECenter4@Andys-iMac-2.local>
2015-03-23 11:58:33 -0400

    Initial Commit

---
+++ GPS Tester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPS Tester
+//
+//  Created by Chris Bennett on 3/23/15.
+//  Copyright (c) 2015 Chris Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GPS Tester/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPS Tester
+//
+//  Created by Chris Bennett on 3/23/15.
+//  Copyright (c) 2015 Chris Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
